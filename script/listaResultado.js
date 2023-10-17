4168282eeedfc057077afb3d38ef7e547a6708d7
AlefdeJesus <alefjesus1996st.s@gmail.com>
2023-10-16 21:44:38 -0300

    tabela

--- script/listaResultado.js
+++ script/listaResultado.js
@@ -1,39 +1,61 @@
 function listaResultado() {
     const resultado = document.getElementById('resultados');
-  
-    const intervaloDePesquisa = 5000; // Intervalo de 5 segundos (ajuste conforme necessário)
+    const intervaloDePesquisa = 5000;
   
     const buscarDados = () => {
       fetch('http://localhost:3333/home')
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
           resultado.innerHTML = ''; // Limpe o conteúdo atual
+  
+          const gridContainer = document.createElement('div');
+          gridContainer.id = 'quadrados-container';
+  
+          let divRow = document.createElement('div');
+          divRow.className = 'linha-quadrados';
+  
           data.forEach((element) => {
             const classCor = element.cor;
             const seBrancoSeNumero =
               classCor === 'branco'
                 ? '<img src="./image/branco.svg" alt="sinal branco" class="img-branco">'
                 : element.numero;
-            const listar = `
+            const divResultado = document.createElement('div');
+            divResultado.className = 'resultadoDoJs';
+            divResultado.innerHTML = `
+       
+            <div id="resultadoDoJs">
               <div class="caixa ${classCor}">
                 <div class="circulo">
                   <span>${seBrancoSeNumero}</span>
                 </div>
               </div>
-              <div id="data-hora">${element.data} ${element.hora}</div>
+              <div class="hora">${element.hora}</div>
+            </div>
+
             `;
-            resultado.innerHTML += listar;
+  
+            divRow.appendChild(divResultado);
+  
+            if (divRow.childElementCount >= 5) {
+              gridContainer.appendChild(divRow);
+              divRow = document.createElement('div');
+              divRow.className = 'linha-quadrados';
+            }
           });
+  
+          if (divRow.childElementCount > 0) {
+            gridContainer.appendChild(divRow);
+          }
+  
+          resultado.appendChild(gridContainer);
         })
         .catch((error) => {
           console.error(error);
         });
-    };
+    }
   
-    buscarDados(); // Execute a primeira busca imediatamente
-  
-    // Defina um timeout para buscar dados periodicamente
+    buscarDados();
     setInterval(buscarDados, intervaloDePesquisa);
   }
   
